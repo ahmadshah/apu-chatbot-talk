bd7897cfa60cfad39bbe577e61755b9935b95114
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2018-04-01 05:24:26 +0800

    add firebase example
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

--- index.js
+++ index.js
@@ -2,34 +2,66 @@ require('dotenv').config()
 
 const TelegramBot = require('node-telegram-bot-api');
 const DialogFlow = require('apiai');
+
+const Firebase = require('firebase');
+require("firebase/firestore");
+
 const TelegramToken = process.env.TELEGRAM_TOKEN;
 const DialogFlowToken = process.env.DIALOGFLOW_TOKEN;
 const NlpSessionID = process.env.DIALOGFLOW_SESSION_ID;
 const Bot = new TelegramBot(TelegramToken, {polling: true});
 const Nlp = DialogFlow(DialogFlowToken);
 
+Firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID
+});
+const DB = Firebase.firestore();
+
 Bot.onText(/\/start/, (msg) => {
     Bot.sendMessage(msg.chat.id, "Hello there, Let's chat!");
 });
 
+Bot.onText(/\/intake (.+)/, (msg, match) => {
+    var programme = match[1].toLowerCase().replace(/ /g, '-');
+    
+    DB.collection('intakes')
+        .doc(programme)
+        .get()
+        .then((doc) => {
+            if(doc.exists) {
+                Bot.sendMessage(msg.chat.id, "Commencement dates for the programe are " + doc.data().commencement_date.join(", "));
+            } else {
+                Bot.sendMessage(msg.chat.id, "Sorry, I cant seem to find the related programme.");
+            }
+        })
+        .catch((error) => {
+            console.log('Error querying document');
+        });
+});
+
 Bot.on('message', (msg) => {
-    var request = Nlp.textRequest(msg.text.toString(), {
-        sessionId: NlpSessionID
-    });
+    var text = msg.text.toString();
+    if (text.indexOf('/') == -1) {
+        var request = Nlp.textRequest(msg.text.toString(), {
+            sessionId: NlpSessionID
+        });
 
-    request.on('response', function (response) {
-        var answer = response.result.fulfillment.speech;
+        request.on('response', function (response) {
+            var answer = response.result.fulfillment.speech;
 
-        if(answer == '') {
-            Bot.sendMessage(msg.chat.id, "Sorry, I can't answer that. Let's talk about something else.");
-        } else {
-            Bot.sendMessage(msg.chat.id, answer);
-        }
-    });
+            if (answer == '') {
+                Bot.sendMessage(msg.chat.id, "Sorry, I can't answer that. Let's talk about something else.");
+            } else {
+                Bot.sendMessage(msg.chat.id, answer);
+            }
+        });
 
-    request.on('error', function (error) {
-        Bot.sendMessage(msg.chat.id, "Sorry, I can't answer that.");
-    });
+        request.on('error', function (error) {
+            Bot.sendMessage(msg.chat.id, "Sorry, I can't answer that.");
+        });
 
-    request.end();
+        request.end();
+    }
 });
